85d247071bc9b99200ffd9aabd18fa67eaa8ee1f
Hadad Al Gojali <hadadalgojali@gmail.com>
2019-10-04 16:19:25 +0700

    Update validasi Middleware/AuthKey.php

--- app/Http/Middleware/AuthKey.php
+++ app/Http/Middleware/AuthKey.php
@@ -16,11 +16,14 @@ class AuthKey
      */
     public function handle($request, Closure $next)
     {
-      if (strlen($request->header('APP_KEY')) > 5) {
+      // if (strlen($request->header('APP_KEY')) > 5) {
+      if (strlen($request->header()['api-key'][0]) > 5) {
         return response()->json(['code' => 401, 'message' => 'APP KEY Failed formatted !', ], 401);
       }
-      $result = Api_token::where('secret_key',$request->header('APP_KEY'))
-      ->where('api_token', md5($request->header('API_TOKEN')))
+      // $result = Api_token::where('secret_key',$request->header('APP_KEY'))
+      $result = Api_token::where('secret_key',$request->header()['api-key'][0])
+      // ->where('api_token', md5($request->header('API_TOKEN')))
+      ->where('api_token', md5($request->header()['app-token'][0]))
       ->count();
 
       if($result>0){
